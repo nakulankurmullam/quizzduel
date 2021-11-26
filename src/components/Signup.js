355888a77d6985f8a0c6391bb27bca4fa4a337d3
nakulankurmullam <nakulankurmullam66@gmail.com>
2021-11-26 22:15:20 +0530

    signup component finished

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,11 +1,27 @@
 import React from "react";
+import "./Signup.css";
 
 function Signup() {
   return (
-    <div>
-       This is the modal signup 
+    <div className="signup-container">
+      <div className="modal-box">
+        <div className="header">
+          <h2>Sign Up</h2>
+          <span id="close-btn">&times;</span>
+        </div>
+        <form className="modal-content">
+          <label htmlFor="#username">Choose a user name: </label>
+          <input type="text" placeholder="username" required />
+          <label htmlFor="#email">Email: </label>
+          <input type="text" placeholder="example123@email.com" id="email" required />
+          <label htmlFor="#pswd">Create password: </label>
+          <input id="pswd" type="password" placeholder="password" required />
+          <label htmlFor="#pswd-confirm">Confirm password: </label>
+          <input type="password" id="pswd-confirm" placeholder="confirm password" required />
+          <button id="submit-btn">SUBMIT</button>
+        </form>
+      </div>
     </div>
   );
 }
-
 export default Signup;
